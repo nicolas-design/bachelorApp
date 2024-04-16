901bd4aad40b7333b75c3828b6dce7cea197605e
Nicolas.G <gadnernicolas@gmail.com>
2024-04-17 00:10:37 +0200

    only get calls from last 24 hours

--- android/app/src/main/kotlin/com/example/ba_app_flutter_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ba_app_flutter_1/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import android.provider.CallLog
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.example.ba_app_flutter_1/call_log"
@@ -22,9 +23,26 @@ class MainActivity: FlutterActivity() {
     }
 
     private fun getOutgoingCallsCount(): Int {
-        val cursor = contentResolver.query(android.provider.CallLog.Calls.CONTENT_URI, null, android.provider.CallLog.Calls.TYPE + " = ?", arrayOf(android.provider.CallLog.Calls.OUTGOING_TYPE.toString()), null)
-        val count = cursor?.count ?: 0
-        cursor?.close()
-        return count
-    }
+    // Calculate the timestamp for 24 hours ago
+    val oneDayAgoMillis = System.currentTimeMillis() - (24*60*60 * 1000)
+    
+
+    // Define the selection criteria for the query to get only outgoing calls from the last 24 hours
+    val selection = "${CallLog.Calls.TYPE} = ? AND ${CallLog.Calls.DATE} >= ?"
+    val selectionArgs = arrayOf(CallLog.Calls.OUTGOING_TYPE.toString(), oneDayAgoMillis.toString())
+
+    // Query the call log with the specified selection criteria
+    val cursor = contentResolver.query(
+        CallLog.Calls.CONTENT_URI,
+        null,   // You can specify specific columns you need here
+        selection,
+        selectionArgs,
+        null
+    )
+
+    // Count the number of records in the cursor
+    val count = cursor?.count ?: 0
+    cursor?.close()
+    return count
+}
 }
